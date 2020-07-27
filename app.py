5b94908fc2fc40e796653b94cb16839446fadb2e
Tarun Krishnan <62337153+tarun36rocker@users.noreply.github.com>
2020-07-27 12:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -8,23 +25,31 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
+def model_predict(img_path, model):
+    test_image = image.load_img(img_path, target_size=(64, 64))
+    test_image=image.img_to_array(test_image) #converts it into 3d array
+    test_image=np.expand_dims(test_image,axis=0)
+    prediction = model.predict(test_image)
+    
+    return prediction
+
 
 @app.route('/predict',methods=['GET', 'POST'])
 def predict():
-    from keras.preprocessing import image
-    '''
-    For rendering results on HTML GUI
-    '''
     if request.method == 'POST':
-        test_image=image.load_img(request.files['pic'],target_size = (64, 64))
-        test_image=image.img_to_array(test_image) #converts it into 3d array
-        test_image=np.expand_dims(test_image,axis=0)
-        prediction = model.predict(test_image)
+        # Get the file from post request
+        f = request.files['file']
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        # Make prediction
+        preds = model_predict(file_path, model)
         if(prediction[0]==0):
             output="Cat!!"
         else:
-            output="Dog!!" 
-    
+            output="Dog!!"
 
     return render_template('index.html', prediction_text='Your animal is a : {}'.format(output))
 
