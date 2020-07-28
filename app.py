c28e5bce1b4ab1ed40cd02ac3aeecb80cde757fc
Tarun Krishnan <62337153+tarun36rocker@users.noreply.github.com>
2020-07-28 21:06:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+ from __future__ import division, print_function
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+
+model = pickle.load(open('model.pkl', 'rb'))   
+import os
+
+from flask import Flask, render_template, request
+
+from ocr import ocr_core
+
+
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def model_predict(img_path, model):
+    test_image = image.load_img(img_path, target_size=(64, 64))
+    test_image=image.img_to_array(test_image) #converts it into 3d array
+    test_image=np.expand_dims(test_image,axis=0)
+    prediction = model.predict(test_image)
+    return prediction
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            return render_template('index.html', prediction_text='File Error ! ')
+        file = request.files['file']
+
+        if file.filename == '':
+            return render_template('index.html', prediction_text='File Error ! ')
+
+        if file and allowed_file(file.filename):
+            file_path=os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+            prediction = model_predict(file_path, model)
+            if(prediction[0]==0):
+                output="Cat!!"
+            else:
+                output="Dog!!"
+
+            return render_template('index.html', prediction_text='Your animal is a : {}'.format(output))
+   
+
+if __name__ == '__main__':
+    app.run(debug = True)
+    
