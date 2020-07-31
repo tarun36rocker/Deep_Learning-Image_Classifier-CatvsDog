d057d90fc60b61ecf4bb9dccfa68abdff143efb0
Tarun Krishnan <62337153+tarun36rocker@users.noreply.github.com>
2020-07-31 14:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def predict():
             else:
                 output="Dog!!"
                 pic='pic=https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*'
-            return render_template('final.html', prediction_text='Your animal is a : {}'.format(output),pic)
+            return render_template('final.html', prediction_text='Your animal is a : {}'.format(output),pic=pic)
             
 
 
